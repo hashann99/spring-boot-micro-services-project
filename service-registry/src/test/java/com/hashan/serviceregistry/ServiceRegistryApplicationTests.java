137cedab2841ffa6e6ef403738b41a828b22df36
hashann99 <hashann99@gmail.com>
2022-12-03 13:10:24 +0530

    create service-registry with eureka

---
+++ service-registry/src/test/java/com/hashan/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashan.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
